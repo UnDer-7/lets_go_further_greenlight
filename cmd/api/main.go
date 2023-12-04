b439d4173a7f7341ff4399905e0bc3ce80a66338
UnDer-7 <mateus7532@gmail.com>
2023-12-03 22:59:15 -0300

    chapter 13.5

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,7 @@ import (
 	"log/slog"
 	"net"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -44,6 +45,7 @@ type application struct {
 	logger *slog.Logger
 	models data.Models
 	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
