83edef9a1763b31c0c7f4f4564bae2744af45590
UnDer-7 <mateus7532@gmail.com>
2024-06-04 19:58:01 -0300

    chapter 19.4 module Proxies and Vendoring

--- Makefile
+++ Makefile
@@ -39,10 +39,7 @@ db/migration/up: confirm
 
 ## audit: tidy dependencies and format, vet and test all code
 .PHONY: audit
-audit:
-	@echo 'Tidying and verifying module dependencies...'
-	go mod tidy
-	go mod verify
+audit: vendor
 	@echo 'Formatting code...'
 	go fmt ./...
 	@echo 'Vetting code...'
@@ -50,3 +47,12 @@ audit:
 	staticcheck ./...
 	@echo 'Running testes...'
 	go test -race -vet=off ./...
+
+## vendor: tidy and vendor dependencies
+.PHONY: vendor
+vendor:
+	@echo 'Tidying and verifying module dependencies...'
+	go mod tidy
+	go mod verify
+	@echo 'Vendoring dependencies...'
+	go mod vendor
--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,10 +4,10 @@ import (
 	"errors"
 	"expvar"
 	"fmt"
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 	"greenlight.mateus.cardoso.com/internal/data"
 	"greenlight.mateus.cardoso.com/internal/validator"
-	"net"
 	"net/http"
 	"strconv"
 	"strings"
@@ -62,11 +62,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if app.config.limiter.enable {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			ip := realip.FromRequest(r)
 
 			mu.Lock()
 
